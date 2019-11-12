9fd21116cff8a9679359a724158690fcf8a2fa19
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2019-11-12 16:38:51 +0100

    Dodaj pliki projektów.

---
+++ UsingDictionaries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsingDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
