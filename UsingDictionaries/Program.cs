bc60042a699c070e4030715a8de19883ac693210
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-10 14:55:11 +0100

    It still does nothing

--- UsingDictionaries/Program.cs
+++ UsingDictionaries/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using static System.Console;
+using System.Threading;
+using UsingDictionaries;
+using System.Collections.Generic;
 
 namespace UsingDictionaries
 {
+    
+
     class Program
     {
+        public static void Silnia(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                i *= n;
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread t1 = new Thread(() => Silnia(100));
+            Dictionary<int, Thread> NT = new Dictionary<int, Thread>
+            {
+                //NT.Add(0, t0);
+                { 1, t1 }
+            };
         }
     }
 }
